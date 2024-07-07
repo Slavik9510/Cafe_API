da9eb3c4c7b698edca5658ea49d70211ec59cf7d
Slvaik9510 <slavik123496@gmail.com>
2024-07-07 12:22:52 +0300

    completed client home page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { TopPanelComponent } from './top-panel/top-panel.component';
+import { HotDealsComponent } from './hot-deals/hot-deals.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, TopPanelComponent],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    TopPanelComponent,
+    HotDealsComponent,
+    HomeComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ client/src/app/hot-deals/hot-deals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hot-deals',
+  standalone: true,
+  imports: [],
+  templateUrl: './hot-deals.component.html',
+  styleUrl: './hot-deals.component.scss'
+})
+export class HotDealsComponent {
+
+}
